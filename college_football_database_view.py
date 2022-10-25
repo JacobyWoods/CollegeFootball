76b91c782e116917311e6318f141b8a35bf04c93
JacobyWoods <jcoby6@gmail.com>
2022-10-24 21:39:44 -0600

    Add cfbd api.

--- college_football_database_view.py
+++ college_football_database_view.py
@@ -3,13 +3,19 @@ import pandas as pd
 
 
 def college_football_database_view():
-    pass
 
+    con = sqlite3.connect("Database/cfb.db")
+    '''These are the tables in the database:
+    cfb2013  cfb2014  cfb2015  cfb2016  cfb2017  cfb2018  cfb2019  cfb2020
+    '''
 
+    df = pd.read_sql_query('SELECT * FROM "cfb2020"', con)
+    pd.set_option('display.max_columns', None)
 
+    df.to_csv('Database/2020_all.csv')
 
+    print(df.describe())
 
 if __name__ == '__main__':
 
-
-    college_football_database_view
+    college_football_database_view()
